f29650d324fde32d6cf4d0d6f619239fea2b6636
buschidos <sergey.snjozyk@gmail.com>
2015-04-23 14:39:06 +0300

    update

--- site/js/common.js
+++ site/js/common.js
@@ -1,13 +1,36 @@
 head.ready(function() {
 
-    console.log('Hello from the Hell');
+        $('#switch-view').on('click', function() {
+            $('#screen-container').toggleClass('is-switched');
+        });
 
-    $('#switch-view').on('click', function() {
-        $('#screen-container').toggleClass('is-switched');
-    });
+        $('.menu-btn').on('click', function() {
+            $('body').toggleClass('is-menu-open');
+        });
 
-    $('.menu-btn').on('click', function() {
-        $('body').toggleClass('is-menu-open');
-    });
+        if ($('.js-dd').length) {
+            $('.js-dd').each(function() {
+                var drop     = $(this),
+                    selected = drop.find('.js-dd-selected');
+                    select   = drop.find('select');
 
+                function setSelected() {
+                    var text = drop.find('select option:selected').text();
+                    if ( selected[0].tagName == 'INPUT' ) {
+                        // text = select.val();
+                        selected.val(text);
+                        selected.change();
+                    } else {
+                        // text = drop.find('select option:selected').text();
+                        selected.text(text);
+                    }
+                }
+
+                setSelected();
+
+                select.on('change', function() {
+                    setSelected();
+                });
+            });
+        }
 });
