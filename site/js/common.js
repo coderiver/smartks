ced3a2c56c269f388ffc3c0347066b58386d0c2d
buschidos <sergey.snjozyk@gmail.com>
2015-04-24 17:56:37 +0300

    update

--- site/js/common.js
+++ site/js/common.js
@@ -4,8 +4,20 @@ head.ready(function() {
             $('#screen-container').toggleClass('is-switched');
         });
 
-        $('.menu-btn').on('click', function() {
-            $('body').toggleClass('is-menu-open');
+        $('.menu-btn').on('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            $('body').addClass('is-menu-open');
+            overlay.show();
+            $('.out').triggerHandler('menu-open');
+        });
+
+        $('.out').bind('menu-open', function(event) {
+            $(this).bind('click', function() {
+                 $('body').removeClass('is-menu-open');
+                 overlay.hide();
+                 $(this).unbind('click');
+            });
         });
 
         if ($('.js-dd').length) {
@@ -17,11 +29,9 @@ head.ready(function() {
                 function setSelected() {
                     var text = drop.find('select option:selected').text();
                     if ( selected[0].tagName == 'INPUT' ) {
-                        // text = select.val();
                         selected.val(text);
                         selected.change();
                     } else {
-                        // text = drop.find('select option:selected').text();
                         selected.text(text);
                     }
                 }
@@ -33,4 +43,77 @@ head.ready(function() {
                 });
             });
         }
+
+        var overlay = {
+
+            isVisible :  false,
+            className : "is-visible",
+            element   : $('.page-overlay'),
+            show: function() {
+                if ( !this.isVisible ) {
+                    this.element.addClass(this.className);
+                    this.isVisible = true;
+                }
+            },
+            hide: function() {
+                if ( this.isVisible ) {
+                    this.element.removeClass(this.className);
+                    this.isVisible = false;
+                }
+            },
+            toggle: function() {
+                if ( this.isVisible ) {
+                    this.hide();
+                } else {
+                    this.show();
+                }
+            }
+
+        };
+
+        $('.header .search-btn').on('click', function(event) {
+            event.preventDefault();
+            $('.header .search').toggleClass('is-visible');
+            overlay.toggle();
+        });
+
+        //accordion
+        (function() {
+
+            var accordion = $('.accordion');
+
+            if ( accordion.length ) {
+
+                var itemSelector    = '.accordion__item',
+                    btnSelector     = '.accordion__title',
+                    contentSelector = '.accordion__content',
+                    activeClass     = 'is-active';
+
+                accordion.each(function() {
+                    var el       = $(this),
+                        btn      = el.find(btnSelector),
+                        items    = el.find(itemSelector),
+                        contents = el.find(contentSelector);
+
+                    btn.on('click', function(event) {
+                        event.preventDefault();
+
+                        var currentBtn = $(this),
+                            item       = currentBtn.parent(itemSelector),
+                            content    = currentBtn.siblings(contentSelector);
+
+                        if ( item.hasClass(activeClass) ) {
+                            item.removeClass(activeClass);
+                            content.slideUp(200);
+                        } else {
+                            items.removeClass(activeClass);
+                            contents.slideUp(200);
+                            item.addClass(activeClass);
+                            content.slideDown(200);
+                        }
+                    });
+                });
+            }
+
+        })();
 });
